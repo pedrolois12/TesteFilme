6910fff16b1b4d60df1668ca0b66cec22676e3e3
Pedro <pedroferreiralois@gmail.com>
2025-02-02 14:21:58 -0300

    major changes
    Update tab3.page.html
    
    Revert "Update tab3.page.html"
    
    This reverts commit 53712e417962163a422da8b3c3d4d7af72a059af.
    
    Revert "Revert "Update tab3.page.html""
    
    This reverts commit 3da1cc108177e6c9ecef6b884c2cebd61c7678e0.
    
    Revert "Revert "Revert "Update tab3.page.html"""
    
    This reverts commit 47bceeab725f62936e6c339682c29d7b13662dc7.
    
    Atualizando pendencias do projeto
    
    1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
    firebaseConfig:{
-    apiKey: "AIzaSyDB8_NUXrIudujAgtOFBLzAbzi8M4KVALk",
-    authDomain: "covid19-ff0ed.firebaseapp.com",
-    databaseURL: "https://covid19-ff0ed.firebaseio.com",
-    projectId: "covid19-ff0ed",
-    storageBucket: "covid19-ff0ed.appspot.com",
-    messagingSenderId: "664711803582",
-    appId: "1:664711803582:web:a4e9499f682a7f961da4ef"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
     } 
 
   
